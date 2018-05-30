062ba8ffdf80972c379e6cc94034b27a0d3a1ffc
undefinido <undefinido@gmail.com>
2018-05-30 17:51:07 -0300

    lmao

--- server.py
+++ server.py
@@ -11,120 +11,132 @@ import time
 # wallet.py
 # MAM.py
 
-class TaskList:
-    def __init__(self):
-        self.task_dict = {}
-        pass
+class Task:
+    def __init__(self, id_t, total_t, ignore_addr = False):
+        self.id_t = id_t
+        self.total_t = total_t
+        self.body = ['' for i in range(self.total_t)]
+        self.mask = [False for i in range(self.total_t)]
+        self.addr = ''
 
-    def _get_info(self, tag):
-        return map(int, tag.split('|'))
+        self.mask[0] = ignore_addr
 
-    def _save_task_to_file(self, id_t):
-        r = open(str(id_t) + '.py', 'w')
+    def get_id(self):
+        return self.id_t
 
-        first = True
+    def get_len(self):
+        return self.total_t
 
-        for l in self.task_dict[id_t]:
-            if first:
-                first = False
-                continue
+    def get_received(self):
+        i = 0
+        for m in self.mask:
+            if m:
+                i +=1
 
-            r.write(l)
-
-        r.close()
-
-    def show(self):
-        for id_t in self.task_dict:
-            print 'Task: ', id_t
-            print 'Content:'
-            for c in self.task_dict[id_t]:
-                print c
-            print '-------------------------------'
-
-    def is_new_task(self, id_t):
-        return id_t not in self.task_dict.keys()
-
-    def test_content_number(self, id_t):
-        print '++++++++', self.task_dict[id_t][-1], len(self.task_dict[id_t])
-        return self.task_dict[id_t][-1] < len(self.task_dict[id_t])
-
-    def check_task(self, tag):
-        # True if we NEED to check the tangle
-        id_t, index_t, total_t = self._get_info(tag)
-
-        # already received task. check if we have all its content.
-        if not self.is_new_task(id_t):
-            # True if we are LEFT of content.
-            return self.test_content_number(id_t)
-
-        # New task
-        return True
-
-    def add_task(self, id_str, line_code):
-        id_t, index_t, total_t = self._get_info(id_str)
-
-        id_t, index_t, total_t = int(id_t), int(index_t), int(total_t)
-
-        if self.is_new_task(id_t):
-            # last position is a counter
-            body = ['' for i in range(total_t)] + [0]
-
-            body[index_t] = line_code
-            # inc counter
-            body[-1] += 1
-
-            self.task_dict[id_t] = body
-        else:
-            self.task_dict[id_t][index_t] = line_code
-            self.task_dict[id_t][-1] += 1
-
-    def list_tasks(self):
-        # There is a header.
-        for id_t in self.task_dict:
-            print 'id_t: {0} | total: {1} received: {2}'.format(id_t, \
-                    len(self.task_dict[id_t]), self.task_dict[id_t][-1])
-
-class Client:
-    def __init__(self, addr):
-        self.time = 0
-        self.addr = addr
-        self.task = None
+        return i
 
     def get_addr(self):
         return self.addr
 
-    def get_task_id(self):
-        return self.task_id
+    def set_addr(self, addr):
+        self.addr = addr
 
-    def set_time(self, time):
-        self.time = time
+    def set_msg_at_index(self, index_t, msg_t, overwrite = False):
+        # if not to overwrite.. leave.
+        if self.mask[index_t] and (not overwrite):
+            print 'Not overwriting.'
+            return None
 
-    def set_task_id(self, task_id):
-        self.task_id = task_id
+        self.body[index_t] = msg_t
+        self.mask[index_t] = True
 
-    def get_time(self):
-        return self.time
+    def is_content_left(self):
+        for m in self.mask:
+            if m == False:
+                return True
 
-    def get_price(self):
-        pass
+        return False
 
-class HandleClients:
+    def get_content(self):
+        return self.body
+
+class TaskList:
     def __init__(self):
-        self.client_list = []
-        pass
+        self.task_list = []
 
-    def add_client(self, client):
-        self.client_list.append(client)
+    def _get_info(self, tag):
+        return map(int, tag.split('|'))
 
-    def remove_client(self, client):
-        pass
+    def _get_task_with_id(self, id_t):
+        for task in self.task_list:
+            if task.get_id() == id_t:
+                return task
 
-    def is_new_task(self, client):
-        for c in self.cleint_list:
-            if c.get_task_id() == client.get_task_id():
+        return None
+
+    def show(self):
+        for task in self.task_list:
+            id_t = task.get_id()
+            print 'Task: ', id_t
+            print 'Content:'
+            print 'Addr: |', task.get_addr()[:10], '|'
+            for c in task.get_content():
+                print c
+            print '-------------------------------'
+
+    def is_new_task(self, task):
+        for t in self.task_list:
+            if t.get_id() == task.get_id():
                 return False
 
         return True
+
+    def is_content_left(self, task):
+        return task.is_content_left()
+
+    def check_tag(self, tag):
+        # True if we NEED to check the tangle
+        id_t, index_t, total_t = self._get_info(tag)
+
+        r = self._get_task_with_id(id_t)
+
+        # New task
+        if r == None:
+            return True
+
+        # Is there any content left?
+        return r.is_content_left()
+
+    def add_task(self, tag, msg):
+        id_t, index_t, total_t = self._get_info(tag)
+        id_t, index_t, total_t = int(id_t), int(index_t), int(total_t)
+
+        # Create a task
+        task = Task(id_t, total_t, ignore_addr = True)
+
+        # Is it a new task?
+        if self.is_new_task(task):
+            if index_t == 0:
+                task.set_addr(msg)
+            else:
+                task.set_msg_at_index(index_t, msg)
+
+            self.task_list.append(task)
+        else:
+            # Update (if possible) existing task.
+            task = self._get_task_with_id(id_t)
+            if index_t == 0:
+                task.set_addr(msg)
+            else:
+                task.set_msg_at_index(index_t, msg)
+
+    def list_tasks(self):
+        # There is a header.
+        for task in self.task_list:
+            id_t = task.get_id()
+
+            print 'id_t: {0} | total: {1} received: {2}'.format(task.get_id(), \
+                    task.get_len(), task.get_received())
 
 def verify_tangle(iota, tasks):
     txn_list = iota.find_transactions()
@@ -136,11 +148,9 @@ def verify_tangle(iota, tasks):
         t = TryteString(tag_t)
         tag = t.decode()
 
-        if tasks.check_task(tag):
+        if tasks.check_tag(tag):
             m = TryteString(msg_t)
             msg = m.decode()
-
-            print '--------', tag, msg[:10]
 
             tasks.add_task(tag, msg)
         else:
